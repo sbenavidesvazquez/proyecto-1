d7df4f353bb138c0ce8bb5d4fe6c952bf7008b75
sbenavidesvazquez <sbenavidesvazquez@eniac22.danielcastelao.org>
2014-10-02 09:47:27 +0200

    ensalada

--- javaapplication/JavaApplication.java
+++ javaapplication/JavaApplication.java
@@ -16,7 +16,7 @@ public class JavaApplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        // Soy el fucking master of the univers
+        // no conquistas nada, con una ensalada.
     }
     
 }
